20ad6ab29aa7c66a701c27b2a585f5ae53e7c1b6
kker <907765631@qq.com>
2020-11-10 16:00:00 +0800

    [Mamingjun] feat: implement practice2

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,11 @@
-const parseData
+const parseData = ({data, column}) => {
+  return data.reduce((acc, cur) => {
+    const data = {};
+    for(let item of column) {
+      data[item.name] = cur[column.indexOf(item)];
+    }
+    acc.push(data);
+    return acc;
+  }, []);
+}
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,12 @@
-const inject
+const inject = (items, sections) => {
+  return sections
+    .sort((a, b) => a.insex - b.index)
+    .reduce(
+      (acc, cur) => {
+        acc.splice(acc.indexOf(items[cur.index]), 0, cur.content);
+        return acc;
+      },
+      [...items]
+    );
+};
 export { inject };
